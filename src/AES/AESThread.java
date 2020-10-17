2eabc90c02ede84222b4bf49f8380e2f6da7348f
David Alberto Mena <davidmenam15@gmail.com>
2020-10-16 23:45:34 -0500

    Update AESThread.java

--- src/AES/AESThread.java
+++ src/AES/AESThread.java
@@ -24,23 +24,23 @@ public class AESThread extends Thread{
     }
 
     // Constructor
-    public AESThread(String targetKey, String searchKey, int numIterations, int keyLen) {
+    public AESThread(String targetKey, String searchKey, int numIterationsAlg, int keyLen, int numOfRuns) {
         this.targetKey = targetKey;
         this.searchKey = searchKey; // 16 bytes o 128 bits
-        this.mainKey = mainKey;
-        this.numIterations = numIterations;
+        this.numIterationsAlg = numIterationsAlg;
         this.keyLen = keyLen;
+        this.numOfRuns = numOfRuns;
         this.salt = generateSalt();
     }
 
     // Attributes
     // To generate a password-based encryption (PBE), based on user input
-    private String targetKey;
+    private String targetKey; // password from which to generate or decrypt the keys
     private String searchKey;
-    private String mainKey; // password from which to generate the keys
     private String salt; // to prevent dictionary attacks
-    private int numIterations; // adds rounds to algorithm, making it more secure, but computationally heavier
+    private int numIterationsAlg; // adds rounds to algorithm, making it more secure, but computationally heavier
     private int keyLen;
+    private int numOfRuns;
     private byte[] iv; // initial vector of bytes, later implemented
     // Selecting type of encryption and details
     private String algorithmName= "AES";
@@ -58,14 +58,11 @@ public class AESThread extends Thread{
     public String getSecretKeyFactoryAlgorithm() {
         return secretKeyFactoryAlgorithm;
     }
-    public String getmainKey() {
-        return mainKey;
-    }
     public String getSalt() {
         return salt;
     }
-    public int getNumIterations() {
-        return numIterations;
+    public int getNumIterationsAlg() {
+        return numIterationsAlg;
     }
     public int getKeyLen() {
         return keyLen;
@@ -73,22 +70,25 @@ public class AESThread extends Thread{
     public byte[] getIv() {
         return iv;
     }
+    public int getNumOfRuns() {
+        return numOfRuns;
+    }
 
     // Setter Methods
-    public void setmainKey(String plainText) {
-        this.mainKey = plainText;
+    public void setSalt() { // random
+        this.salt = generateSalt();
     }
-    public void setSalt(String salt) {
-        this.salt = salt;
-    }
-    public void setNumIterations(int numIterations) {
-        this.numIterations = numIterations;
+    public void setNumIterationsAlg(int numIterationsAlg) {
+        this.numIterationsAlg = numIterationsAlg;
     }
     public void setKeyLen(int keyLen) {
         this.keyLen = keyLen;
     }
     public void setIv(byte[] iv) {
         this.iv = iv;
+    }
+    public void setNumOfRuns(int numOfRuns) {
+        this.numOfRuns = numOfRuns;
     }
 
     // Methods:
@@ -100,10 +100,13 @@ public class AESThread extends Thread{
     public String encrypt(String plainText) throws Exception{
         // Generate the key
         byte[] saltBytes = salt.getBytes("UTF-8");
-        SecretKeyFactory skfact = SecretKeyFactory.getInstance(this.secretKeyFactoryAlgorithm);
-        PBEKeySpec keySpec = new PBEKeySpec(this.targetKey.toCharArray(), saltBytes, this.numIterations, this.keyLen);
+        SecretKeyFactory skfact = SecretKeyFactory.getInstance(
+                this.secretKeyFactoryAlgorithm);
+        PBEKeySpec keySpec = new PBEKeySpec(this.targetKey.toCharArray(),
+                saltBytes, this.numIterationsAlg, this.keyLen);
         SecretKey secretKey = skfact.generateSecret(keySpec);
-        SecretKeySpec formatKey = new SecretKeySpec(secretKey.getEncoded(), algorithmName);
+        SecretKeySpec formatKey = new SecretKeySpec(secretKey.getEncoded(),
+                algorithmName);
         // Start AES for encryption
         Cipher cipher = Cipher.getInstance(blockType);
         cipher.init(Cipher.ENCRYPT_MODE, formatKey); // is it missing the iv?
@@ -118,15 +121,16 @@ public class AESThread extends Thread{
      * @return decrypted text
      * @throws Exception
      */
-    public String decrypt(String cipherText, String inputKey) throws Exception {
+    public String decrypt(String cipherText) throws Exception {
         byte[] saltBytes = salt.getBytes("UTF-8");
         byte[] encryptTextBytes = new Base64().decode(cipherText);
-
-        SecretKeyFactory skf = SecretKeyFactory.getInstance(this.secretKeyFactoryAlgorithm);
-        PBEKeySpec spec = new PBEKeySpec(this.searchKey.toCharArray(), saltBytes, this.numIterations, this.keyLen);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(
+                this.secretKeyFactoryAlgorithm);
+        PBEKeySpec spec = new PBEKeySpec(this.searchKey.toCharArray(),
+                saltBytes, this.numIterationsAlg, this.keyLen);
         SecretKey secretKey = skf.generateSecret(spec);
-        SecretKeySpec formatKey = new SecretKeySpec(secretKey.getEncoded(), algorithmName);
-
+        SecretKeySpec formatKey = new SecretKeySpec(secretKey.getEncoded(),
+                algorithmName);
         //decrypt the message
         Cipher cipher = Cipher.getInstance(blockType);
         cipher.init(Cipher.DECRYPT_MODE, formatKey, new IvParameterSpec(iv));
@@ -134,17 +138,65 @@ public class AESThread extends Thread{
         byte[] decyrptTextBytes = null;
         try {
             decyrptTextBytes = cipher.doFinal(encryptTextBytes);
+//            System.out.println("decyrptTextBytes ");
+//            System.out.println(decyrptTextBytes);
         } catch (IllegalBlockSizeException e) {
             e.printStackTrace();
         } catch (BadPaddingException e) {
-            e.printStackTrace();
+            System.err.println("It is a wrong key");
         }
         String text = new String(decyrptTextBytes);
         return text;
     }
 
-    /** En proceso ...
-     * Falta implementar el metodo run con las iteraciones respectivas de descencripción
-     * Por último, poner como variables algunos parámetros en el código que se dejaron quemados para fines de pruebas
+    /**
+     * Increment key + 1
      */
+    private void incrementKey(){
+        // turn string to char array
+        char[] charArray = this.searchKey.toCharArray();
+        // turn each int into corresponding bits String
+        int n = charArray.length;
+        for (int i = 0; i < n; i++){
+            int numCh = charArray[i]; // each char as num
+            numCh = (((numCh-97) + 1) % 26)+97;
+            charArray[i] = (char) numCh; // (-97) in range of lower letters
+        }
+        // update search key
+        String newKey = new String(charArray);
+        this.searchKey = newKey;
+        System.out.println("Llave out ");
+        System.out.println(newKey);
+        System.out.println("Llave out 2");
+    }
+
+    /**
+     * Main function runned in the thread
+     * Iterates through the brute force attack decryptions of current thread
+     */
+    public void run(){
+        // Encrypt
+        String cipherText = "";
+        try{
+            cipherText = this.encrypt(this.targetKey);
+        } catch(Exception except){
+                except.printStackTrace();
+                System.out.println("except.printStackTrace()");
+        }
+
+        System.out.println("cipherText");
+        System.out.println(cipherText);
+        System.out.println("Llave in");
+        System.out.println(this.searchKey);
+        // run decryptions
+        for (int i = 0; i < this.numOfRuns; i++){
+            try{
+                this.decrypt(cipherText);
+            } catch(Exception except){
+                except.printStackTrace();
+            }
+            incrementKey();
+        }
+    }
+
 }
